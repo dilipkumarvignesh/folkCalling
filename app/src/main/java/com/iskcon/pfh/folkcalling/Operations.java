f0f89c366cde82397348c7b8acb644553e2a36c0
dilipkumarvignesh <dilipkumarvignesh@gmail.com>
2017-09-05 14:33:02 +0530

    Added one more activity

---
+++ app/src/main/java/com/iskcon/pfh/folkcalling/Operations.java
@@ -0,0 +1,13 @@
+package com.iskcon.pfh.folkcalling;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Operations extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_operations);
+    }
+}
